781a5ba77c3e3cd627e367f333623116fa1bccec
dforrester20549 <123339615+dforrester20549@users.noreply.github.com>
2023-10-29 17:46:33 -0600

    ProjectConfig
    Se terminan de realizar las configuraciones necesarias con el @Bean e importar las librerias para mejorar el codigo

--- Reposteria/src/main/java/com/reposteria/ProjectConfig.java
+++ Reposteria/src/main/java/com/reposteria/ProjectConfig.java
@@ -5,27 +5,30 @@
 package com.reposteria;
 
 import java.util.Locale;
-import org.springframework.boot.autoconfigure.web.WebProperties.LocaleResolver;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
+@Configuration
 public class ProjectConfig implements WebMvcConfigurer{
     /*los siguientes metodos son para incorporar el tema de internacionalizacion*/
     
     /*localeResolver se utiliza para crear una sesion de cambio de idioma*/
     
-//    @Bean 
-//    public LocaleResolver localeResolver(){
-//        slr.setDefaultLocale(defaultLocale:Locale.getDefault());
-//        slr.setLocalAttributeName(localAttributeName: "session.current.locale");
-//        slr.setTimeZoneAttibuteName(timeZoneAttributeName:"session.current.time");
-//        return slr;
-//    }
+    @Bean 
+    public LocaleResolver localeResolver(){
+        var slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.getDefault());
+        slr.setLocaleAttributeName("session.current.locale");
+        slr.setTimeZoneAttributeName("session.current.timezone");
+        return slr;
+    }
     
     /*localeChangeInterceptor se utliza para crear un interceptro de idioma*/
     
